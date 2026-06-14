213de101a6da99d569d566554849c173fc7733ba
khyathivanga06-spec <khyathivanga06@gmail.com>
2026-06-14 18:02:02 +0000

    .# Please enter the commit message for your changes. Lines starting

--- utils.py
+++ utils.py
@@ -1,59 +1,33 @@
 import streamlit as st
 
 def load_css():
+
     st.markdown("""
     <style>
+
     .stApp {
         background: #0B1020;
     }
-    </style>
-    """, unsafe_allow_html=True)
 
-def hero_banner(title, subtitle):
+    section[data-testid="stSidebar"] {
 
-    st.markdown(f"""
-    <div style="
-    background:linear-gradient(
-    135deg,
-    #7C3AED,
-    #06B6D4,
-    #14B8A6
-    );
-    padding:35px;
-    border-radius:25px;
-    text-align:center;
-    margin-bottom:25px;
-    box-shadow:0px 0px 30px rgba(6,182,212,0.3);
-    ">
-
-    <h1 style="color:white;">
-    {title}
-    </h1>
-
-    <p style="color:white;font-size:18px;">
-    {subtitle}
-    </p>
-
-    </div>
-    """, unsafe_allow_html=True)
-    st.markdown("""
-    <style>
-
-    .stApp {
-        background: #0B1020;
+        background: linear-gradient(
+            180deg,
+            #111827,
+            #1E293B,
+            #0F172A
+        );
     }
 
     section[data-testid="stSidebar"] [aria-current="page"] {
 
-    background: linear-gradient(
-        135deg,
-        #8B5CF6,
-        #06B6D4
-    ) !important;
+        background: linear-gradient(
+            135deg,
+            #8B5CF6,
+            #06B6D4
+        ) !important;
 
-    border-radius: 18px;
-}
-        );
+        border-radius: 18px;
     }
 
     .stButton > button {
@@ -85,4 +59,33 @@ def hero_banner(title, subtitle):
     }
 
     </style>
+    """, unsafe_allow_html=True)
+
+
+def hero_banner(title, subtitle):
+
+    st.markdown(f"""
+    <div style="
+    background:linear-gradient(
+    135deg,
+    #7C3AED,
+    #06B6D4,
+    #14B8A6
+    );
+    padding:35px;
+    border-radius:25px;
+    text-align:center;
+    margin-bottom:25px;
+    box-shadow:0px 0px 30px rgba(6,182,212,0.3);
+    ">
+
+    <h1 style="color:white;">
+    {title}
+    </h1>
+
+    <p style="color:white;font-size:18px;">
+    {subtitle}
+    </p>
+
+    </div>
     """, unsafe_allow_html=True)
